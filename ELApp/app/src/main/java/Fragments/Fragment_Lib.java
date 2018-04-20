72426eed3c5ac6c19e3a8b6d52a03cf66c73d432
韩虚虹 <hxh@hanxuhongdeMacBook-Pro.local>
2018-04-20 19:49:54 +0800

    。。
    。。

--- ELApp/app/src/main/java/Fragments/Fragment_Lib.java
+++ ELApp/app/src/main/java/Fragments/Fragment_Lib.java
@@ -6,6 +6,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.lenovo.elapp.R;
+
 public class Fragment_Lib extends AppCompatActivity {
 
     /**
@@ -20,6 +22,7 @@ public class Fragment_Lib extends AppCompatActivity {
         fragmentTransaction.replace(Root_frame_layout, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+        int c=R.drawable.ic_launcher_background;
     }
 //
 //    public static void replaceFragment(Fragment fragment) {
