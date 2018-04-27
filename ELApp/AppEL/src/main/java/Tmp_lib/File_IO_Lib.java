2c8ce7a2d00fca1e75a74f2a232fdb29351f1f23
陆放明 <1071956678@qq.com>
2018-04-27 09:39:55 +0800

    4.27

--- ELApp/AppEL/src/main/java/Tmp_lib/File_IO_Lib.java
+++ ELApp/AppEL/src/main/java/Tmp_lib/File_IO_Lib.java
@@ -5,21 +5,51 @@ import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class File_IO_Lib {
+//    public static String[] getAssetsAbsolutePath(Context context, String string) {
+//
+//    }
 
     /**
      * Get the inputStream of the assets file
+     * You could use this to get the info from the files
      */
     public static InputStream getAssets(Context context, String string) throws IOException {
-        return context.getAssets().open(string);
+        return context.getResources()
+                .getAssets()
+                .open(string);
+
     }
 
     public static Uri getUriRes(Context context, int rawFile) {
+
         return Uri.parse("android.resource://" + context.getPackageName() + "/" + rawFile);
+    }
+
+    public static String getAssetsInfo(Context context, String string) {
+        try {
+            return new String(InputStreamToByte(getAssets(context, string)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static byte[] InputStreamToByte(InputStream is) throws IOException {
+        ByteArrayOutputStream bytestream = new ByteArrayOutputStream();
+        int ch;
+        while ((ch = is.read()) != -1) {
+            bytestream.write(ch);
+        }
+        byte imgdata[] = bytestream.toByteArray();
+        bytestream.close();
+        return imgdata;
     }
 
     /**
@@ -58,5 +88,56 @@ public class File_IO_Lib {
             file.mkdirs();
         }
         return filePath;
+    }
+
+    /**
+     * 从assets目录中复制整个文件夹内容
+     *
+     * @param context Context 使用CopyFiles类的Activity
+     * @param oldPath String  原文件路径  如：/aa
+     * @param newPath String  复制后路径  如：xx:/bb/cc
+     */
+    public void copyFilesFassets(Context context, String oldPath, String newPath) {
+        try {
+            String fileNames[] = context.getAssets().list(oldPath);//获取assets目录下的所有文件及目录名
+            if (fileNames.length > 0) {//如果是目录
+                File file = new File(newPath);
+                file.mkdirs();//如果文件夹不存在，则递归
+                for (String fileName : fileNames) {
+                    copyFilesFassets(context, oldPath + "/" + fileName, newPath + "/" + fileName);
+                }
+            } else {//如果是文件
+                InputStream is = context.getAssets().open(oldPath);
+                FileOutputStream fos = new FileOutputStream(new File(newPath));
+                byte[] buffer = new byte[1024];
+                int byteCount = 0;
+                while ((byteCount = is.read(buffer)) != -1) {//循环从输入流读取 buffer字节
+                    fos.write(buffer, 0, byteCount);//将读取的输入流写入到输出流
+                }
+                fos.flush();//刷新缓冲区
+                is.close();
+                fos.close();
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            //如果捕捉到错误则通知UI线程
+        }
+    }
+
+
+    public static byte[] WriteSomething(Context context, String string) {
+        byte[] bytes = new byte[10];
+        try {
+            InputStream inputStream = context.getResources()
+                    .getAssets()
+                    .open("music/bgm4.mp3");
+            bytes = new byte[20];
+            inputStream.read(bytes, 0, 5);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bytes;
     }
 }
--- ELApp/testlfm/src/main/java/com/example/testlfm/Music_lib.java
+++ ELApp/testlfm/src/main/java/com/example/testlfm/Music_lib.java
@@ -1,5 +1,4 @@
 package com.example.testlfm;
-
 /*
 *1. To get the source
 * a. 用户在应用中事先自带的resource资源
@@ -13,34 +12,64 @@ c. 网络上的媒体文件
 //尚未实现某一个序列音乐播放
 //暂时未考虑service  ——2018.4.18
 
+/*
+*一个button同时实现暂停播放和继续播放，需要在activity里加一个private boolean字段IsPause
+* 此外，方法play不能够放在“实现暂停、继续”的回调函数或者事件监听器中（不然的话还是会从头开始播放）
+* play方法属于确定了mediaplayer的来源，并且播放，请放在有关于音乐选择的地方
+* if (IsPause) {
+                mediaPlayer = Music_lib.ContinueToPlay(mediaPlayer);
+                IsPause = false;
+            } else {
+                mediaPlayer = Music_lib.pause(mediaPlayer);
+                IsPause = true;
+            }
+*——4.21
+* 注：
+* */
+
+/*
+ * 实现了一个函数Play直接同时播放assets和res内的文件
+ * 同时让mediaPlayer的创建完全由一个函数完成
+ * ———4.26
+ * */
+
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Environment;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Music_lib {
-
-    public static MediaPlayer GetMediaPlayer(Context context, int rawSource) {
-        return MediaPlayer.create(context, rawSource);
-    }
-
+    /**
+     * Get the mediaPlayer,and you have to choose the source of the music.
+     */
     public static MediaPlayer GetMediaPlayer() {
         return new MediaPlayer();
     }
 
-    public static MediaPlayer play(MediaPlayer mediaPlayer) {
-        if (mediaPlayer.isPlaying()) {
-            mediaPlayer.start();
-            return mediaPlayer;
-        }
-        mediaPlayer.reset();
-        mediaPlayer.prepareAsync();
-        mediaPlayer.setOnPreparedListener(mp -> mediaPlayer.start());
+    public static MediaPlayer play(Context context, MediaPlayer mediaPlayer, int rawFile) {
+        mediaPlayer = playRes(context, mediaPlayer, rawFile);
         return mediaPlayer;
     }
 
-    public static MediaPlayer play(MediaPlayer mediaPlayer, String source) {
+    public static MediaPlayer play(Context context, MediaPlayer mediaPlayer, String source) {
+        try {
+            mediaPlayer = playAssets(context, mediaPlayer, source);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return mediaPlayer;
+
+    }
+
+    /**
+     * @param source :The external source(like in the sdcards)
+     */
+    public static MediaPlayer playExternal(MediaPlayer mediaPlayer, String source) {
 
         try {
             if (mediaPlayer == null) {
@@ -97,41 +126,104 @@ public class Music_lib {
         return mediaPlayer;
     }
 
-    public static MediaPlayer LoopPlay(MediaPlayer mediaPlayer, String source, boolean pro) {
+    /**
+     * @param pro :True if you want to play the music loop.
+     */
+    public static MediaPlayer LoopPlay(Context context, MediaPlayer mediaPlayer,
+                                       String source, boolean pro) {
         mediaPlayer.setLooping(pro);
         mediaPlayer.setOnCompletionListener(mp -> {
-            if (pro) play(mediaPlayer, source);
+            if (pro) {
+                play(context, mediaPlayer, source);
+            } else stop(mediaPlayer);
+        });
+        return mediaPlayer;
+    }
+
+    public static MediaPlayer LoopPlay(Context context, MediaPlayer mediaPlayer,
+                                       int rawFile, boolean pro) {
+        mediaPlayer.setLooping(pro);
+        mediaPlayer.setOnCompletionListener(mp -> {
+            if (pro) {
+                play(context, mediaPlayer, rawFile);
+            } else stop(mediaPlayer);
+        });
+        return mediaPlayer;
+    }
+
+    public static MediaPlayer LoopPlayExternal(MediaPlayer mediaPlayer, String source, boolean pro) {
+        mediaPlayer.setLooping(pro);
+        mediaPlayer.setOnCompletionListener(mp -> {
+            if (pro) playExternal(mediaPlayer, source);
             else stop(mediaPlayer);
         });
         return mediaPlayer;
     }
 
-    public static MediaPlayer ChangeToPlayAnother(MediaPlayer mediaPlayer, String source) {
+    public static MediaPlayer ChangeToPlayAnotherExternal(MediaPlayer mediaPlayer, String source) {
         mediaPlayer = Music_lib.stop(mediaPlayer);
-        mediaPlayer = Music_lib.play(mediaPlayer, source);
+        mediaPlayer = Music_lib.playExternal(mediaPlayer, source);
         return mediaPlayer;
     }
 
-    public static MediaPlayer ChangeToPlayAnother(Context context, MediaPlayer mediaPlayer, int R_source) {
-        Music_lib.stop(mediaPlayer);
-        mediaPlayer = MediaPlayer.create(context, R_source);
+    public static MediaPlayer ChangeToPlayAnother(Context context, MediaPlayer mediaPlayer
+            , String string) {
+        mediaPlayer = Music_lib.stop(mediaPlayer);
+        mediaPlayer = Music_lib.play(context, mediaPlayer, string);
+        return mediaPlayer;
+    }
+
+    public static MediaPlayer ChangeToPlayAnother(Context context, MediaPlayer mediaPlayer
+            , int rawFile) {
+        mediaPlayer = Music_lib.stop(mediaPlayer);
+        mediaPlayer = Music_lib.play(context, mediaPlayer, rawFile);
+        return mediaPlayer;
+    }
+
+    /**
+     * Get the mediaPlayer from the assets/music.
+     *
+     * @param context     :The Context to play the music.
+     * @param mediaPlayer :The Target mediaPlayer.
+     * @param string      :The file name.
+     */
+    private static MediaPlayer playAssets(Context context, MediaPlayer mediaPlayer, String string)
+            throws IOException {
+        if (mediaPlayer == null) {
+            mediaPlayer = Music_lib.GetMediaPlayer();
+        }
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+            return mediaPlayer;
+        }
+        mediaPlayer.reset();
+        AssetManager am = context.getAssets();
+        AssetFileDescriptor afd = am.openFd("music/" + string);
+        mediaPlayer.setDataSource(afd.getFileDescriptor());
+        mediaPlayer.prepareAsync();
+        MediaPlayer finalMediaPlayer = mediaPlayer;
+        mediaPlayer.setOnPreparedListener(mp -> finalMediaPlayer.start());
+        return mediaPlayer;
+    }
+
+    private static MediaPlayer playRes(Context context, MediaPlayer mediaPlayer, int rawFile) {
+        if (mediaPlayer == null) {
+            mediaPlayer = Music_lib.GetMediaPlayer();
+        }
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+            return mediaPlayer;
+        }
+        mediaPlayer.reset();
+        try {
+            Uri uri = Uri.parse("android.resource://" + context.getPackageName() + "/" + rawFile);
+            mediaPlayer.setDataSource(context, uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.prepareAsync();
+        MediaPlayer finalMediaPlayer = mediaPlayer;
+        mediaPlayer.setOnPreparedListener(mp -> finalMediaPlayer.start());
         return mediaPlayer;
     }
 }
-
-
-/*
-*一个button同时实现暂停播放和继续播放，需要在activity里加一个private boolean字段IsPause
-* 此外，方法play不能够放在“实现暂停、继续”的回调函数或者事件监听器中（不然的话还是会从头开始播放）
-* play方法属于确定了mediaplayer的来源，并且播放，请放在有关于音乐选择的地方
-* if (IsPause) {
-                mediaPlayer = Music_lib.ContinueToPlay(mediaPlayer);
-                IsPause = false;
-            } else {
-                mediaPlayer = Music_lib.pause(mediaPlayer);
-                IsPause = true;
-            }
-*——4.21
-* 注：
-* */
-
