37c1fd4603fd087d6f8b9edbce3eb64e2baa9115
陆放明 <1071956678@qq.com>
2018-04-30 08:18:05 +0800

    4.30  图片加载基本完成

--- ELApp/testlfm/src/main/java/com/example/testlfm/MainActivity.java
+++ ELApp/testlfm/src/main/java/com/example/testlfm/MainActivity.java
@@ -2,68 +2,52 @@ package com.example.testlfm;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Transformation;
-
-import java.io.File;
-import java.util.Timer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button=findViewById(R.id.image1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!IsPermitted()){
-                    requestPermissions();
-                }else {
-                    mediaPlayer=Music_lib.GetMediaPlayer();
-                    Music_lib.play(mediaPlayer,"Taylor Swift-End Game.mp3");
-                        try {
-                            new Thread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    try {
-                                        new timer(10);
-                                        Music_lib.pause(mediaPlayer);
-                                    } catch (InterruptedException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }).start();
+        Button button = findViewById(R.id.play);
+        button.setOnClickListener(v -> {
+            if (!IsPermitted()) {
+                requestPermissions();
+            } else {
+                mediaPlayer = Music_lib.GetMediaPlayer();
+                Music_lib.playExternal(mediaPlayer, "bgm6.mp3");
+                try {
+                    new Thread(() -> {
+                        TextView textView = findViewById(R.id.text);
+                        Music_lib.pause(mediaPlayer);
+                    }).start();
 
-                        }catch (Exception e) {
-                            e.printStackTrace();
-                    }
-                    //System.out.println(3);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
-    }
 
-    public void ToImage1(View view) {
 
     }
 
     public void ToImage2(View view) {
+        ImageView imageView = findViewById(R.id.imageView);
+        Image_IO.SetImage(MainActivity.this,R.raw.new_loading,imageView);
     }
+
 
     private boolean IsPermitted() {
         return ContextCompat.checkSelfPermission(MainActivity.this
