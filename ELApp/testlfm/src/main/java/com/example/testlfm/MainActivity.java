7508fd32897f72f5853be83685caf8a9ef41cf4c
陆放明 <1071956678@qq.com>
2018-05-06 22:12:35 +0800

    添加了两种loading动画模块orz

--- ELApp/testlfm/src/main/java/com/example/testlfm/MainActivity.java
+++ ELApp/testlfm/src/main/java/com/example/testlfm/MainActivity.java
@@ -4,33 +4,51 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 import cn.iwgang.countdownview.CountdownView;
+import io.github.yuweiguocn.lib.squareloading.SquareLoading;
+import site.gemus.openingstartanimation.NormalDrawStrategy;
+import site.gemus.openingstartanimation.OpeningStartAnimation;
+import site.gemus.openingstartanimation.RotationDrawStrategy;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
     private boolean IsPause = false;
     private CountdownView countdownView;
+    private SquareLoading squareLoading;
+    // 1. 定义控件变量
+    private scut.carson_ho.kawaii_loadingview.Kawaii_LoadingView kawai;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        squareLoading = loading_lib.getSquareLoading(this, R.id.squareLoading);
+//        squareLoading = loading_lib.square_loading_stop(squareLoading);
+        kawai = loading_lib.getkawaii_loadingView(this, R.id.Kawaii_LoadingView);
+
+
     }
 
     public void playAndCount(View view) {
-
+//        squareLoading=loading_lib.square_loading_start(squareLoading);
         if (countdownView == null) {
             countdownView = CountingDown.getCountingDown(MainActivity.this, R.id.countingDown);
         }
-        countdownView = CountingDown.start(30, countdownView);
+        countdownView = CountingDown.start(3, 5, countdownView);
         countdownView.setOnCountdownEndListener(cv -> {
             mediaPlayer = Music_lib.pause(mediaPlayer);
             IsPause = true;
         });
-        TextView textView = findViewById(R.id.text);
         if (!File_IO_Lib.IsPermitted(MainActivity.this)) {
             File_IO_Lib.requestPermissions(MainActivity.this);
         } else {
@@ -38,13 +56,31 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer = Music_lib.GetMediaPlayer();
                 mediaPlayer = Music_lib.play(MainActivity.this, mediaPlayer, "bgm2.mp3");
                 IsPause = false;
+                loading_lib.kawaii_loading_start(kawai);
             } else if (IsPause) {
                 mediaPlayer = Music_lib.ContinueToPlay(mediaPlayer);
                 IsPause = false;
+                loading_lib.kawaii_loading_start(kawai);
             } else {
                 mediaPlayer = Music_lib.pause(mediaPlayer);
                 IsPause = true;
+                loading_lib.kawaii_loading_stop(kawai);
+                countdownView = CountingDown.pause(countdownView);
             }
         }
+    }
+
+    public void btn2Event(View view) {
+        OpeningStartAnimation openingStartAnimation = new OpeningStartAnimation.Builder(this)
+                .setDrawStategy(new NormalDrawStrategy())
+                .create();
+        openingStartAnimation.show(this);
+    }
+
+    public void btn3Event(View view) {
+        OpeningStartAnimation openingStartAnimation3 = new OpeningStartAnimation.Builder(this)
+                .setDrawStategy(new RotationDrawStrategy())
+                .create();
+        openingStartAnimation3.show(this);
     }
 }
