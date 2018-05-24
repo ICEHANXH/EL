84f34aff59434184a46cda580609ae0dc9096726
James <1025149919@qq.com>
2018-05-25 05:09:19 +0800

    副本最后一页搞定 连接到NewTask

--- ELApp/AppEL/src/main/java/Story/storySeventhActivity.java
+++ ELApp/AppEL/src/main/java/Story/storySeventhActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.lenovo.elapp.NewTaskActivity;
 import com.example.lenovo.elapp.R;
 
 import Activitys.MainActivity;
@@ -25,7 +26,7 @@ public class storySeventhActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =  new Intent(storySeventhActivity.this, MainActivity.class);
+                Intent intent =  new Intent(storySeventhActivity.this, NewTaskActivity.class);
                 startActivity(intent);
             }
         });
@@ -33,7 +34,7 @@ public class storySeventhActivity extends AppCompatActivity {
 
 //    /**Called when the user clicks the Send button*/
 //    public void sendMessage(View view){
-//        Intent intent =  new Intent(storySeventhActivity.this, MainActivity.class);
+//        Intent intent =  new Intent(storySeventhActivity.this, NewTaskActivity.class);
 //        startActivity(intent);
 //    }
 }
